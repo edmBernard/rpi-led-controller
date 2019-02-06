f27c3bc06991c69ef995c60fc3be5c32872dac78
Erwan BERNARD <bernard.erwan@gmail.com>
2019-02-06 15:15:01 +0100

    add color wheel

--- client/js/app.js
+++ client/js/app.js
@@ -7,6 +7,11 @@ var square_size = board_size / dim;
 var border_grid_size = 1
 var square_list = []
 
+var color_wheel = []
+for (let i = 0; i < 360; i+=20) {
+    color_wheel.push("hsl(" + i/360 + ", 100%, 50%)");
+}
+
 function turn_off() {
     for (let i = 0; i < square_list.length; i++) {
         square_list[i].attr({fill: "#000000"});
@@ -15,7 +20,7 @@ function turn_off() {
 
 function turn_on() {
     for (let i = 0; i < square_list.length; i++) {
-        square_list[i].attr({fill: "#4286f4"});
+        square_list[i].attr({fill: color_wheel[square_list[i].color_idx - 1]});
     }
 }
 
@@ -53,10 +58,17 @@ var s = Snap("#svg");
 for (let i = 0; i < dim; i++) {
     for (let j = 0; j < dim; j++) {
         let square = s.rect(i * square_size, j * square_size, square_size - border_grid_size, square_size - border_grid_size);
-        square.attr({fill: '#4286f4'});
+        square.attr({fill: color_wheel[0]});
+        square.color_idx = 1;  // Inject color index
+
         square_list.push(square);
+
         square.click(function () {
-            square.attr({fill: "#000000"})
+            if (square.color_idx >= color_wheel.length)
+                square.color_idx = 0;
+            console.log(square.color_idx);
+            console.log(color_wheel[square.color_idx]);
+            square.attr({fill: color_wheel[square.color_idx++]});
         });
     }
 }
