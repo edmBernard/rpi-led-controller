47a8782754a53c14c71c446175d7ec82e7fa5da8
Erwan BERNARD <bernard.erwan@gmail.com>
2019-02-06 19:16:29 +0100

    add server

---
+++ server/app.py
@@ -0,0 +1,33 @@
+import json
+from fire import Fire
+from bottle import Bottle, request, response, run, static_file
+
+app = Bottle()
+
+
+@app.route("/<filename:path>")
+def do_static(filename):
+    """
+        Sert les fichiers statiques
+    """
+    return static_file(filename, root="../client/")
+
+@app.route("/")
+def do_static():
+
+    return static_file("index.html", root="../client/")
+
+
+
+@app.route("/udpate/")
+def do_update():
+
+    return "Data updated"
+
+
+def main(port, server="tornado"):
+    run(app, host="0.0.0.0", port=port, server=server)
+
+
+if __name__ == "__main__":
+    Fire(main)
